4aea7d45c02a40c2eb3e392918604ac937290c03
Carmen <carmencantudo@gmail.com>
2023-02-17 22:23:18 +0000

    Add NGO list view and url path

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('listview/', views.NGOList.as_view(), name='listview'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import NGO
+
 
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
 
+
+class NGOList(ListView):
+    """
+    List all NGOs
+    """
+    model = NGO
+    template_name = 'listview.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(NGOList, self).get_context_data(*args, **kwargs)
+        return context
